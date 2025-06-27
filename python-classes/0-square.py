883369ebc446e7cd4247e3655fc7cc390e0e7ddc
zehraadgozelo <zehraadgozelova.com>
2025-06-27 19:47:17 +0000

    s

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-
 """Square class modulunu təsvir edən sənəd (docstring)."""
+
+
 class Square:
     """Kvadrat sinfi."""
-    def __init__(self,size):
-        """Kvadratin olcusunu teyin ele"""
+
+    def __init__(self, size):
+        """Kvadratin ölçüsünü təyin edir."""
         self.__size = size
