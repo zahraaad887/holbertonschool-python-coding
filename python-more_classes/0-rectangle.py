9c6663ac2f171afca07b1a72641e1d9252e13221
zahraadgozel <zehraadgozelova@example.com>
2025-06-28 12:21:24 +0000

    s

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""Square class modulunu təsvir edən sənəd string)."""
+
+
+class Rectangle:
+    """Kvadrat sinfi."""
+
+    def __init__(self, size):
+        """Kvadrarin olcu."""
+        self.__size=size
